26a329901369a59309b5374fc345515065d03f1d
SamJakob <SamJakob@users.noreply.github.com>
2021-12-01 04:17:38 +0000

    deploy: c594fd02df5309964c22b703dec74a9658034d62

---
+++ _nuxt/static/1638332257/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638332257",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c41dd16c"}}}}(null,"\u002F"));
