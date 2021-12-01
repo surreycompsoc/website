989090467ad497335a9f1e960a37ea9c1ccfa5b2
SamJakob <SamJakob@users.noreply.github.com>
2021-12-01 03:36:23 +0000

    deploy: f218872b843c296fa371bb6fbc090bd5293bdf01

---
+++ _nuxt/static/1638329782/committee/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638329782",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcommittee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"227faa13"}}}}(null));
