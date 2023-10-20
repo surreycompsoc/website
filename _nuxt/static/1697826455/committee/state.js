389f5a683d8c299987858b49114244c33505916a
AranJannson <AranJannson@users.noreply.github.com>
2023-10-20 18:27:37 +0000

    deploy: 1d9c28ced5bfc105db53ce6feab50216b2c64ddf

---
+++ _nuxt/static/1697826455/committee/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697826455",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcommittee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3e9e1b05"}}}}(null));
