dba3abee67068b496fc76609c59d7bd2600bb6c7
AranJannson <AranJannson@users.noreply.github.com>
2023-12-12 12:33:19 +0000

    deploy: 14657059c098c621c5d82b968b6387f5d8bcafd1

--- _nuxt/static/1698054051/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1698054051",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"af369b19"}}}}(null,"\u002F"));
