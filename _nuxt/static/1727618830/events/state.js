3b7715a7148f628d44ea44582fa9d16debd6d555
Surrey-CompSoc <Surrey-CompSoc@users.noreply.github.com>
2024-09-29 14:07:11 +0000

    deploy: 09217e9d9c62c2703a28213735140d71071b6f25

---
+++ _nuxt/static/1727618830/events/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1727618830",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevents",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"616d2568"}}}}(null));
