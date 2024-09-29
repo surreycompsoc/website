ef89c8802c842eb47636cc36821a1282cb0778e4
Dhimant-Panchal <Dhimant-Panchal@users.noreply.github.com>
2024-09-29 23:52:49 +0000

    deploy: 2bb10ebe9834dc3ebe28c6056977eea64a46f330

--- _nuxt/static/1727619252/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1727619252",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bfe3e0bb"}}}}(null,"\u002F"));
