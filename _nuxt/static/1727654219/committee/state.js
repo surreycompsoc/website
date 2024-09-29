4129a4ea060f0417863ecec68b9b20827060b06a
Dhimant-Panchal <Dhimant-Panchal@users.noreply.github.com>
2024-09-29 23:57:00 +0000

    deploy: c698f4675856e8d8063e47cd9713882685ea0ff0

---
+++ _nuxt/static/1727654219/committee/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1727654219",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcommittee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"da2e1781"}}}}(null));
---
+++ _nuxt/static/1727654219/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1727654219",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"da2e1781"}}}}(null,"\u002F"));
