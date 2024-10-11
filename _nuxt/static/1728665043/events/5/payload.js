ce3ff9658e0e33519d3ea077cecdb1a41f4e06a6
Dhimant-Panchal <Dhimant-Panchal@users.noreply.github.com>
2024-10-11 16:44:04 +0000

    deploy: a8180672000956fc4316a6c2e35fc4ce3448b059

---
+++ _nuxt/static/1728665043/events/5/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/events/5", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1728665043/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcommittee","\u002Fevents","\u002Fevents\u002F5","\u002Fevents\u002F4","\u002Fevents\u002F3","\u002Fevents\u002F2","\u002Fevents\u002F1"]})
