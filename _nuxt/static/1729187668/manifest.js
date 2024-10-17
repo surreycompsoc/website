2a681f6b04c675a986fb17597ca7b7715358d641
Dhimant-Panchal <Dhimant-Panchal@users.noreply.github.com>
2024-10-17 17:55:09 +0000

    deploy: bd191355e5eccf0a173da88878236b29eb96add6

--- _nuxt/static/1729187668/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fevents","\u002Fcommittee","\u002Fevents\u002F6","\u002Fevents\u002F5","\u002Fevents\u002F4","\u002Fevents\u002F2","\u002Fevents\u002F3","\u002Fevents\u002F1"]})
---
+++ _nuxt/static/1729187707/events/2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1729187707",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevents\u002F2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"12e28b1d"}}}}(null));
